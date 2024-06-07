f2c5ffaf982e3de5742204d90e179a19304c3012
Leonardo Teixeira <leonardobruce1@gmail.com>
2024-06-06 21:43:15 -0300

    implatado while

--- Alura/Lógica de programação - mergulhe em programação com JavaScript/logica-js-projeto_numero_secreto/app.js
+++ Alura/Lógica de programação - mergulhe em programação com JavaScript/logica-js-projeto_numero_secreto/app.js
@@ -1,15 +1,23 @@
 alert('Boas vindas ao jogo do número secreto');
 
-let numeroSecreto = 8;
+let numeroSecreto = 5;
+let chute;
+let tentativas = 1;
 
-let chute = prompt('Escolha um número entre 1 e 10');
-console.log('O número do chute é: ' + chute)
+console.log(numeroSecreto);
+//console.log('Resultado da comparação:', chute == numeroSecreto);
 
-console.log('Resultado da comparação:', chute == numeroSecreto);
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
 
-if (chute == numeroSecreto){
-    alert(`Isso ai! Você descobriu o número secreto: ${numeroSecreto}`);
-}else{
-    alert('Você errou :(')
-    console.log('O número secreto é: ' + numeroSecreto);
+    if (chute == numeroSecreto){
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+    }else{
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        }else{
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        }
+    tentativas ++;
 }
