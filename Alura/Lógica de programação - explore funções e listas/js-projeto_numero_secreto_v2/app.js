d0dc87a564cd58ec7f30f1c89b150a36b2c1ce73
Leonardo Teixeira <leonardobruce1@gmail.com>
2024-06-18 21:19:49 -0300

    Adicionar reiniciar jogo

--- Alura/Lógica de programação - explore funções e listas/js-projeto_numero_secreto_v2/app.js
+++ Alura/Lógica de programação - explore funções e listas/js-projeto_numero_secreto_v2/app.js
@@ -12,8 +12,12 @@ function exibirTextoNaTela(tag, texto){
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 a 10');
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 a 10');
+}
+
+exibirMensagemInicial();
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
@@ -22,6 +26,7 @@ function verificarChute(){
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}, parabéns!`
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }else{
         if(chute > numeroSecreto){
             exibirTextoNaTela('p', 'O número secreto é menor');
@@ -40,4 +45,12 @@ function gerarNumeroAleatorio() {
 function limparCampo(){
     chute = document.querySelector('input');
     chute.value = '';
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
